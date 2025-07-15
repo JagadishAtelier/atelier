6cae6e80b33afe537b7cc5ac283f605eb90f3227
JagadishRamakrishnan <jeevasumathi1357@gmail.com>
2025-07-15 12:10:13 +0530

    updated card tilt animation

--- src/Components/Features/Features.jsx
+++ src/Components/Features/Features.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React,{useEffect} from 'react'
 import './Features.css'
 import Lottie from "lottie-react";
 import animationData from "../../Assets/data.json"; 
 import svgData from '../../Assets/svg.json'
 function Features() {
+  useEffect(() => {
+    const cards = document.querySelectorAll(".easy-item-wrap,.easy-single-wrap._01,.easy-single-wrap");
+
+    cards.forEach((card) => {
+      card.addEventListener("mousemove", (e) => {
+        const rect = card.getBoundingClientRect();
+        const x = e.clientX - rect.left;
+        const y = e.clientY - rect.top;
+
+        const centerX = rect.width / 2;
+        const centerY = rect.height / 2;
+
+        // Rotation sensitivity
+        const rotateX = -(y - centerY) / 10;
+        const rotateY = (x - centerX) / 10;
+
+        card.style.transform = `rotateX(${rotateX}deg) rotateY(${rotateY}deg) scale(1.03)`;
+      });
+
+      card.addEventListener("mouseleave", () => {
+        card.style.transform = "rotateX(0deg) rotateY(0deg) scale(1)";
+      });
+    });
+  }, []);
   return (
     <section className="section easy">
       <div className="w-layout-blockcontainer containers w-container">
